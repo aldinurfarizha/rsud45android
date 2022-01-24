642677a0cb55243292841bbd754c6370da6e77b7
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2022-01-24 15:48:45 +0700

    tinggal daftar

--- app/src/main/java/com/ali/rsud45/Auth/DaftarBaru.java
+++ app/src/main/java/com/ali/rsud45/Auth/DaftarBaru.java
@@ -1,13 +1,14 @@
 package com.ali.rsud45.Auth;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ali.rsud45.R;
 
 public class DaftarBaru extends AppCompatActivity {
-
+EditText nama, nik, tempat_lahir, tanggal_lahir, dusun, rt, rw, no_telp, nama_ibu_kandung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
